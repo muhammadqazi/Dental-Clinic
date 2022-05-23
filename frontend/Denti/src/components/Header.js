3a5c20523b09b2907d325397c9aa6f9f461c5136
hosamalhamwi <hosamalhmawi@gmail.com>
2022-05-22 19:19:30 -0500

    updated header

--- frontend/Denti/src/components/Header.js
+++ frontend/Denti/src/components/Header.js
@@ -1,43 +1,48 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
-import { COLORS, images } from '../constants'
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import {COLORS, images} from '../constants';
 
 const Header = ({containerStyle}) => {
-    return (
-        <View style={{
-            ...containerStyle
-        }}>
-            <View style={{
-                flexDirection: 'row'
-            }}>
-                <Image
-                    source={images.tooth}
-                    style={{
-                        height: 30,
-                        width: 30
-                    }}
-                />
-                <View style={{
-                    marginLeft: 5,
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }}>
-                    <Text style={{
-                        color: COLORS.black,
-                        fontWeight: 'bold',
-                        fontSize: 12,
-                    }}>Dental</Text>
-                    <Text style={{
-                        color: COLORS.black,
-                        fontWeight: 'bold',
-                        fontSize: 8,
-                        textAlign: 'center'
-                    }}>Care</Text>
-                </View>
-
-            </View>
+  return (
+    <View style={[styles.container, containerStyle]}>
+      <View style={styles.innerContainer}>
+        <Image source={images.tooth} style={styles.image} />
+        <View style={styles.textContainer}>
+          <Text style={styles.mainText}>Dental</Text>
+          <Text style={styles.subText}>Care</Text>
         </View>
-    )
-}
+      </View>
+    </View>
+  );
+};
 
-export default Header
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  innerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  image: {
+    height: 30,
+    width: 30,
+  },
+  textContainer: {
+    marginLeft: 5,
+    justifyContent: 'center',
+  },
+  mainText: {
+    color: COLORS.black,
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+  subText: {
+    color: COLORS.black,
+    fontWeight: 'bold',
+    fontSize: 10,
+    textAlign: 'center',
+  },
+});
+
+export default Header;
