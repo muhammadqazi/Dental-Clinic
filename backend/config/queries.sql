a5500153e0e233b427e3230cc079f040759775ba
hosamalhamwi <hosamalhmawi@gmail.com>
2022-05-25 18:27:00 -0500

    updated queries and added tables

--- backend/config/queries.sql
+++ backend/config/queries.sql
@@ -12,32 +12,32 @@ CREATE TABLE doctor (doc_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,   
     --     spec_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
     --     treatmentName varchar(255) NOT NULL,
     --     doc_id int UNSIGNED,
-            
+
     --     FOREIGN KEY (treatmentName) REFERENCES treatment (treatmentName),
     --     FOREIGN KEY (doc_id) REFERENCES doctor (doc_id)
-        
+
     -- );
 
     CREATE TABLE treatment (
-	
+
         treat_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
         treat_name varchar(255) NOT NULL,
         treat_desc varchar(255) NOT NULL,
-        
+
         doc_id INT UNSIGNED,
 
         treat_price varchar(55),
-        
+
         FOREIGN KEY (doc_id) REFERENCES doctor (doc_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
     CREATE TABLE client (
-	
+
         client_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
         firstName varchar(255) NOT NULL,
         lastName varchar(255) NOT NULL,
-        
+
         dateOfBirth date NOT NULL,
         clientAddress varchar(255) NOT NULL	,
         clientTelephone varchar(255)
@@ -48,31 +48,38 @@ CREATE TABLE doctor (doc_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,   
         tel_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
         client_id INT UNSIGNED,
         TelfNo varchar(55) NOT NULL,
-        
+
         FOREIGN KEY (client_id) REFERENCES client (client_id) ON DELETE CASCADE ON UPDATE CASCADE
     );
 
 
 
     CREATE TABLE appointment (
-	
+
         appointment_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
         appointDate date NOT NULL,
         appointTime time NULL,
-            
+
         client_id int UNSIGNED,
         doc_id int UNSIGNED,
         treat_id int UNSIGNED,
         status varchar(45),
-        
+
         FOREIGN KEY (client_id) REFERENCES client (client_id) ON DELETE CASCADE ON UPDATE CASCADE,
         FOREIGN KEY (doc_id) REFERENCES doctor (doc_id) ON DELETE CASCADE ON UPDATE CASCADE,
         FOREIGN KEY (treat_id) REFERENCES treatment (treat_id) ON DELETE CASCADE ON UPDATE CASCADE
     );
 
+    CREATE TABLE appointment_log (
+    log_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    appointment_id INT UNSIGNED,
+    log_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (appointment_id) REFERENCES appointment (appointment_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 
-    
+
+
     CREATE TABLE bill (
         bill_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
         billDate date NOT NULL,
@@ -81,12 +88,12 @@ CREATE TABLE doctor (doc_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,   
         appointment_id int UNSIGNED,
         client_id int UNSIGNED,
 
-    
+
         FOREIGN KEY (appointment_id) REFERENCES appointment (appointment_id) ON DELETE CASCADE ON UPDATE CASCADE,
         FOREIGN KEY (client_id) REFERENCES client (client_id) ON DELETE CASCADE ON UPDATE CASCADE
     );
 
-    
+
 
     CREATE TABLE Payment (
         pay_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
@@ -99,3 +106,33 @@ CREATE TABLE doctor (doc_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,   
         FOREIGN KEY (client_id) REFERENCES client (client_id) ON DELETE CASCADE ON UPDATE CASCADE,
         FOREIGN KEY (bill_id) REFERENCES bill (bill_id) ON DELETE CASCADE ON UPDATE CASCADE
     );
+
+    CREATE TABLE insurance (
+    insurance_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    client_id INT UNSIGNED,
+    provider VARCHAR(255),
+    policy_number VARCHAR(255),
+    FOREIGN KEY (client_id) REFERENCES client (client_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE medical_records (
+    record_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    client_id INT UNSIGNED,
+    doc_id INT UNSIGNED,
+    treatment_id INT UNSIGNED,
+    record_date DATE NOT NULL,
+    notes TEXT,
+    FOREIGN KEY (client_id) REFERENCES client (client_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (doc_id) REFERENCES doctor (doc_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (treatment_id) REFERENCES treatment (treatment_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE treatment_plan (
+    plan_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    client_id INT UNSIGNED,
+    doc_id INT UNSIGNED,
+    plan_description TEXT,
+    created_date DATE NOT NULL,
+    FOREIGN KEY (client_id) REFERENCES client (client_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (doc_id) REFERENCES doctor (doc_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
